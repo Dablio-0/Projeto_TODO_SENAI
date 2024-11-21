7f33f47c25d06640f06594cfa14963557f7bef1d
Wellington de Elias Rodrigues <wellingtonrod.work@gmail.com>
2024-11-21 20:51:28 -0300

    :tada: Commit inicial

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Projeto_TODO_SENAI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Projeto_TODO_SENAI
