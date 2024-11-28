9da800986f69d814aa67d077eb5bd5e12021d576
ViniciusSenaiM <drawnstar46@gmail.com>
2024-11-28 19:22:49 -0300

    atualizando projeto

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-namespace Projeto_TODO_SENAI
+namespace WinFormsApp1
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace Projeto_TODO_SENAI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new ToDoList());
         }
     }
 }
